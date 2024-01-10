0ba891e5d498a1e302b91c0dda3f3f116892d93b
CaioCarvalho <135462331+CaiioCarvalho@users.noreply.github.com>
2024-01-10 17:02:32 -0400

    some changes

---
+++ app/components/index.ts
@@ -0,0 +1,9 @@
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import About from "./About";
+import Testimonials from "./Testimonials";
+
+export { Contact, Navbar, Projects, Skills, About, Testimonials }
+
